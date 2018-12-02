f25378a14602c002d209e2ee6a8994d0b53577b5
pk400 <joelaro.to@gmail.com>
2018-12-02 14:57:50 -0500

    Removed asgi.py

--- asgi.py
+++
@@ -1,8 +0,0 @@
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-
-app = Starlette()
-
-@app.route('/')
-def index(request):
-  return JSONResponse({'message': 'hello world'})
