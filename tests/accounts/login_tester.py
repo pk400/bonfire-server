85727128f9491dc4ef0ea83d6c2365b0d0a92ddf
pk400 <joelaro.to@gmail.com>
2020-02-23 18:03:19 -0500

    Added tests for login.

--- tests/accounts/login_tester.py
+++ tests/accounts/login_tester.py
@@ -1,19 +1,56 @@
+import collections
 import unittest
 
+import bcrypt
+
+from backend import exceptions
 from backend.accounts.account import Account
+from backend.accounts.account_entry import AccountEntry
 from backend.accounts.local_data_store import LocalDataStore
 from backend.accounts.server import Server
 from backend.accounts.session import Session
 from backend.utils import run_in_loop
 
+MockAccountData = \
+  collections.namedtuple('MockAccountData', ['id', 'email_address', 'password'])
+
 class LoginTester(unittest.TestCase):
+  def setUp(self):
+    self.data_store = LocalDataStore()
+    self.server = Server(self.data_store)
+    self.server.open()
+
+  def tearDown(self):
+    self.server.close()
+
   def test_success(self):
-    data_store = LocalDataStore()
-    server = Server(data_store)
-    server.open()
-    run_in_loop(server.create_account('a', 'b'))
-    run_in_loop(server.login(Session(), 'a', 'b'))
-    server.close()
+    data = MockAccountData(99, 'a', 'b')
+    hashed = bcrypt.hashpw(data.password.encode('utf-8'), bcrypt.gensalt())
+    self.data_store._accounts[data.id] = \
+      AccountEntry(Account(data.id, data.email_address), hashed)
+    self.data_store._email_address_to_id[data.email_address] = data.id
+    run_in_loop(self.server.login(Session(), data.email_address, data.password))
+
+  def test_bad_credentials(self):
+    data = MockAccountData(99, 'a', 'b')
+    hashed = bcrypt.hashpw(data.password.encode('utf-8'), bcrypt.gensalt())
+    self.data_store._accounts[data.id] = \
+      AccountEntry(Account(data.id, data.email_address), hashed)
+    self.data_store._email_address_to_id[data.email_address] = data.id
+    with self.assertRaises(exceptions.BadCredentialsException) as error:
+      run_in_loop(self.server.login(Session(), data.email_address, 'bad'))
+    self.assertEqual(error.exception.code, Server.LOGIN_FAILED)
+
+  def test_already_logged_in(self):
+    data = MockAccountData(99, 'a', 'b')
+    hashed = bcrypt.hashpw(data.password.encode('utf-8'), bcrypt.gensalt())
+    self.data_store._accounts[data.id] = \
+      AccountEntry(Account(data.id, data.email_address), hashed)
+    session = Session()
+    session.set_credentials(data.id)
+    with self.assertRaises(exceptions.SessionLoggedInException) as error:
+      run_in_loop(self.server.login(session, data.email_address, data.password))
+    self.assertEqual(error.exception.code, Server.SESSION_LOGGED_IN)
 
 if __name__ == '__main__':
   unittest.main()
